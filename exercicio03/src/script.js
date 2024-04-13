d0426d0a6e3eca00836b38bc17c78b9ed764be92
Bartolomeu Santos <bartolomeusantos072@gmail.com>
2024-04-13 11:50:00 -0300

    update

--- exercicio03/src/script.js
+++ exercicio03/src/script.js
@@ -1,6 +1,6 @@
 alert("Bem-vindo!"); 
 let altura = prompt("informe a altura da lata");
 let raio = prompt("informe o raio da lata");
-const pi=3,14159;
+const pi=3.14159;
 let volume = pi*(raio**2)*altura;
-alert(`O volume é${volume}`);
+alert(`O volume é: ${volume} c`);
