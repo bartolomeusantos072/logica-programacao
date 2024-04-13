170b02d45920d1a4ea5e1f272dc308452fecd541
Bartolomeu Santos <bartolomeusantos072@gmail.com>
2024-04-13 13:50:13 -0300

    corrigir variaveis

---
+++ exercicio12/src/script.js
@@ -0,0 +1,5 @@
+alert("Bem-vindo!"); 
+let reais = prompt("Quantos reais você deseja converter?");
+let cotacao = prompt(“Qual a cotação atual do dollar?”);
+let dollar=reais/cotacao;
+alert(`R$ ${reais} equivale a $ ${dollar}`);
