83c943aafb7cc2577af3fc81e96007519fc1ae90
Bartolomeu Santos <bartolomeusantos072@gmail.com>
2024-04-13 13:53:44 -0300

    nenhuma correção a fazer

---
+++ exercicio13/src/script.js
@@ -0,0 +1,6 @@
+alert("Bem-vindo!"); 
+let valorA = prompt("informe o valor da A");
+let valorB = prompt("informe o valor da B");
+let valorC = prompt("informe o valor da C");
+let soma=(valorA**2)+(valorB**2)+(valorC**2);
+alert(`O valor da soma dos quadrados dos valores A:${valorA}, B:${valorB} e C:${valorC} é igual a ${soma}`);
