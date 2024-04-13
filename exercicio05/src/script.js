233bd235cba2fe04a92baf29218d55c0e63e222f
Bartolomeu Santos <bartolomeusantos072@gmail.com>
2024-04-13 11:55:19 -0300

    correção de fechamento de parenteses

--- exercicio05/src/script.js
+++ exercicio05/src/script.js
@@ -1,1 +1,6 @@
 alert("Bem-vindo!"); 
+let valor = prompt("informe o valor da prestação");
+let taxa = prompt("informe o valor da taxa");
+let tempo=prompt("qual o tempo de atraso?");
+let prestacao = valor+(valor*(taxa/100)*tempo);
+alert(`O valor de uma prestação em atraso é: ${prestacao}`);
