86de3a3af6b75514ade29277e49bcb4e81cc861c
Bartolomeu Santos <bartolomeusantos072@gmail.com>
2024-04-13 14:44:02 -0300

    acrescentera limitador toFixed para casas decimais

--- exercicio03/src/script.js
+++ exercicio03/src/script.js
@@ -3,4 +3,4 @@ let altura = prompt("informe a altura da lata");
 let raio = prompt("informe o raio da lata");
 const pi=3.14159;
 let volume = pi*(raio**2)*altura;
-alert(`O volume é: ${volume} c`);
+alert(`O volume é: ${volume} m³`);
