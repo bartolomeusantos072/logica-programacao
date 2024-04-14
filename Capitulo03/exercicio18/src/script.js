678775f96d5d5e6762d6d72a9255577bffee9345
Bartolomeu Santos <bartolomeusantos072@gmail.com>
2024-04-14 01:12:37 -0300

    mover exercicios para pasta cap03

--- Capitulo03/exercicio18/src/script.js
+++ Capitulo03/exercicio18/src/script.js
@@ -1,14 +1,14 @@
 alert("Bem-vindo!"); 
-let qtdeVotosCandidatoA = Number prompt("informe a quantidade de votos do candidato A?");
-let qtdeVotosCandidatoB = Number prompt("informe a quantidade de votos do candidato B?");
-let qtdeVotosCandidatoC = Number prompt("informe a quantidade de votos do candidato C?");
+let qtdeVotosCandidatoA = Number(prompt("informe a quantidade de votos do candidato A?"));
+let qtdeVotosCandidatoB = Number(prompt("informe a quantidade de votos do candidato B?"));
+let qtdeVotosCandidatoC = Number(prompt("informe a quantidade de votos do candidato C?"));
 
-let qtdeVotosNulo= Number prompt("informe a quantidade de votos Nulos?");
+let qtdeVotosNulo= Number(prompt("informe a quantidade de votos Nulos?"));
 
-let qtdeVotosBranco=Number prompt("informe a quantidade de votos em Branco?");
+let qtdeVotosBranco=Number(prompt("informe a quantidade de votos em Branco?"));
 
 let votosValidos=(qtdeVotosCandidatoA+qtdeVotosCandidatoB+qtdeVotosCandidatoC);
-let totalEleitores= votosValidos+votosNulos+votosBranco;
+let totalEleitores= votosValidos+qtdeVotosNulo+qtdeVotosBranco;
 let percentualVotosValidos=(votosValidos/totalEleitores)*100;
 let percentualVotosCandidatoA=(qtdeVotosCandidatoA/totalEleitores)*100;
 let percentualVotosCandidatoB=(qtdeVotosCandidatoB/totalEleitores)*100;
