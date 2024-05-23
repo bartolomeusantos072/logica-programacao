0f32345b537ecb702d62bc5871b6fcefc1c8eaea
Bartolomeu Uender <bartolomeusantos072@gmail.com>
2024-05-23 00:37:02 -0300

    exercicio3-d

--- Capitulo04/exercicio03-d/src/script.js
+++ Capitulo04/exercicio03-d/src/script.js
@@ -1,4 +1,19 @@
-alert("Bem-vindo!"); 
-let celsius = prompt("informe o valor em C°");
-let fahrenheit = (9*celsius+160)/5;
-alert(`${celsius}C° equivale a ${fahrenheit}F°`);
+let bim1=Number(prompt("Qual a nota do 1°Bim")),
+bim2=Number(prompt("Qual a nota do 2°Bim")),
+bim3=Number(prompt("Qual a nota do 3°Bim")),
+bim4=Number(prompt("Qual a nota do 4°Bim")),
+exame=undefined;
+
+let media = (bim1+bim2+bim3+bim4)/4;
+
+if(media>=7){
+    alert("aluno aprovado");
+}else{
+    exame=Number(prompt("Qual o valor do exame?"));
+    let novaMedia= (media+exame)/2;
+    if(novaMedia>=5){
+        alert("aluno aprovado");
+    }else{
+        alert("aluno reprovado");
+    }
+}
